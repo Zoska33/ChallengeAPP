203a8e76793d9bfae1c922678bddabd06fc38c28
Magdalena Sokolowska <magda60812@wp.pl>
2023-07-27 16:27:21 +0200

    Dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Dzien 4
+
+string name = "Ewa";
+bool woman  = true;
+var age = 18;
+
+if (name == "Ewa" && age < 33)
+{
+    Console.WriteLine("Kobieta ponizej 33lat");
+}
+else if (name =="Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (woman == false && age < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyzna");
+}
